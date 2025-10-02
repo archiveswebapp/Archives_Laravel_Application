ed7249a6d9e3e545829faa66e0456cf6b41efab6
archiveswebapp <archiveswebapp@gmail.com>
2025-10-02 10:41:15 +0530

    Update delete-user-form.blade.php

--- resources/views/livewire/profile/delete-user-form.blade.php
+++ resources/views/livewire/profile/delete-user-form.blade.php
@@ -8,9 +8,9 @@ new class extends Component
 {
     public string $password = '';
 
-    /**
-     * Delete the currently authenticated user.
-     */
+    
+    //Delete the currently authenticated user.
+    
     public function deleteUser(Logout $logout): void
     {
         $this->validate([
